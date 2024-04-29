6c75d52b36cec982dcb3bcd3e8109b09c6d9a10d
Akshay Babu <akshaybabut1@gmail.com>
2024-04-29 16:18:42 +0530

    User  and admin basic curd operation is done

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"github.com/rs/cors"
+	"fmt"
+	"net/http"
+
+	initializer "github.com/akshaybt001/DatingApp_Api_Gateway/Initializer"
 	"github.com/go-chi/chi"
+	"github.com/rs/cors"
 )
 
-func main(){
-	r:=chi.NewRouter()
+func main() {
+	r := chi.NewRouter()
 	r.Use(cors.Default().Handler)
-	
+
+	initializer.Connect(r)
+	fmt.Println("api gateway listening on port 8080")
+	http.ListenAndServe(":8080", r)
 }
